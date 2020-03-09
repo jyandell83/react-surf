1d6a3c3037cb56c9b54358fd8a1ac849528393eb
Jeremy Yandell <jereyand@gmail.com>
2020-03-08 23:42:54 -0700

    add fancy inputs to signup

--- src/Signup/index.js
+++ src/Signup/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Btn, Inpt } from '../globalStyle'
+import { Btn, Inpt, InptGrp, Form } from '../globalStyle'
 
 
 
@@ -24,16 +24,28 @@ const SignUpForm = ({addUser}) =>  {
     console.log(userName,email, passwordOne, passwordTwo)
     
     return(
-        <form onSubmit={e =>  {
+        <Form onSubmit={e =>  {
             e.preventDefault();
             addUser({name: userName, email: email, password: passwordOne});
         }}>
-            <Inpt name="username" placeholder="Username..." value={userName} onChange={e => setUserName(e.target.value)}></Inpt> <br />
-            <Inpt name="email" placeholder="Email..." value={email} onChange={e => setEmail(e.target.value)}></Inpt> <br />
-            <Inpt name="passwordOne" placeholder="passwordOne" value={passwordOne} onChange={e => setPasswordOne(e.target.value)}></Inpt> <br />
-            <Inpt name="passwordTwo" placeholder="passwordTwo" value={passwordTwo} onChange={e => setPasswordTwo(e.target.value)}></Inpt> <br />
+            <InptGrp>
+            <Inpt id="username" name="username" placeholder="Username..." value={userName} onChange={e => setUserName(e.target.value)}></Inpt>
+              <label for="username">Username</label>
+            </InptGrp>
+            <InptGrp>
+            <Inpt id="email" name="email" placeholder="Email..." value={email} onChange={e => setEmail(e.target.value)}></Inpt>
+              <label for="email">Email</label>
+            </InptGrp>
+            <InptGrp>
+            <Inpt id="passwordOne" name="passwordOne" placeholder="passwordOne" value={passwordOne} onChange={e => setPasswordOne(e.target.value)}></Inpt>
+              <label for="passwordOne">Password</label>
+            </InptGrp>
+            <InptGrp>
+            <Inpt id ="passwordTwo" name= "passwordTwo" placeholder="Confirm Password" value={passwordTwo} onChange={e => setPasswordTwo(e.target.value)}></Inpt>
+              <label for="passwordTwo">Confirm Password</label>
+            </InptGrp>
             <Btn type="submit">Sign Up</Btn>
-        </form>
+        </Form>
     )
 
 }
