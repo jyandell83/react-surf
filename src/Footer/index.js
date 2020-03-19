c579f27143efc0a9b78e72fa2642cf169050e4c6
Jeremy Yandell <jereyand@gmail.com>
2020-03-18 21:02:01 -0700

    change class to classname in footer

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -6,7 +6,7 @@ const Footer = () =>  {
     return(
         <Foot>
             <p>Built 100% from scratch by Jeremy Yandell &copy; 2020</p>
-            <div><i class="fab fa-github"></i><i class="fab fa-linkedin"></i></div>
+            <div><i className="fab fa-github"></i><i className="fab fa-linkedin"></i></div>
         </Foot>
     )
 }
